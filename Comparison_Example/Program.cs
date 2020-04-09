cdfc2870c33402cd50b900c6b570cf400b882ca9
Vinicius Belmuds Vasconcelos <belmuds.eng@gmail.com>
2020-04-09 10:59:15 -0300

    Implementação utilizando a interface IComparable

---
+++ Comparison_Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Comparison_Example.Entities;
+namespace Comparison_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List < Product > list = new List<Product>();
+            Product p1 = new Product("TV", 900.00);
+            Product p2 = new Product("Notebook", 1200.00);
+            Product p3 = new Product("Tablet", 450.00);
+            list.Add(p1);
+            list.Add(p2);
+            list.Add(p3);
+
+            list.Sort(); //Icomparable
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
